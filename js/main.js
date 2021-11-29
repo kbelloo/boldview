e242fcca5d1bad74ab4d6acd9b5c221f9894c7e6
Khalid Bello <khalidbello279@gmail.com>
2021-11-29 09:48:23 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//let form = document.getElementById("form");
+//
+//form.addEventListener("submit", function () {
+//
+//    event.preventDefault();
+//
+//    let ourFormData = new FormData(event.target);
+//
+//    let userFirstname = ourFormData.get("fname");
+//
+//    let updatedContent = `
+//            
+//            <h2>Congratulations, ${userFirstname}!</h2>
+//
+//            <p>You're on your way to becoming a BBQ Master!</p>
+//
+//            <p class="content-bottom">We'll never share your information without your permission</p>
+//`
+//    
+//    let mainContent = document.getElementById("content")
+//    
+//    mainContent.innerHTML = updatedContent;
+//
+//});
